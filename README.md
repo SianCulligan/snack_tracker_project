e2374978b09e700816ec9e3653a2337e3270d226
Sian <sianculligan@gmail.com>
2021-03-04 18:13:02 -0800

    added tests and updated readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lab: 27 - Intro to Django
+
+[Open PR](https://github.com/SianCulligan/snack_tracker_project/pull/1)
--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class SnackTest(TestCase):
+# TO RUN TESTS, USE PYTHON MANAGE.PY TEST
+
+# *******************LIST**********************
+    def test_home_page(self):
+        # naming convention 'home' MUST be the same as what's in snack url.py
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_snack_list_page_template(self): 
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_list.html')
+
+# *******************DETAIL**********************
+    # def test_detail_page(self):
+    #     # naming convention 'home' MUST be the same as what's in snack url.py
+    #     url = reverse('snack_detail')
+    #     response = self.client.get(url)
+    #     self.assertEqual(response.status_code, 200)
+
+    # def test_detail_page_template(self): 
+    #     url = reverse('snack_detail')
+    #     response = self.client.get(url)
+    #     self.assertTemplateUsed(response, 'snack_detail.html')
+
+
+# XXX: Test Snack pages
+# XXX: NOTE make sure test extends TestCase instead of SimpleTestCase used in previous class.
+# XXX: verify status code
+# XXX: verify correct template use url name instead of hard coded path TIP: django.urls.reverse will help with that.
+# TODO: We can’t easily test SnackDetailView just yet. Can you figure out why?
+#  Need to somehow pass a pk for snack_detail to work
